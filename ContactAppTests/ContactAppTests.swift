9bc1c1cff30e63302c2b425842326d1ad3f7c7af
Mayank Jangid <226301131@gkv.ac.in>
2025-02-20 17:57:11 +0530

    Initial Commit

---
+++ ContactAppTests/ContactAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppTests.swift
+//  ContactAppTests
+//
+//  Created by Mayank Jangid on 2/20/25.
+//
+
+import Testing
+@testable import ContactApp
+
+struct ContactAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
