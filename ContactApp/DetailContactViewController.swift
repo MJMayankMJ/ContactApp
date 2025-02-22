abe0dfe797ec679b9d573fcf5fbd53274d2d00e4
Mayank Jangid <226301131@gkv.ac.in>
2025-02-22 15:44:28 +0530

    Fixed tab view bar images, detail view, made the keypad buttons round

--- ContactApp/DetailContactViewController.swift
+++ ContactApp/DetailContactViewController.swift
@@ -55,3 +55,28 @@ class DetailContactViewController: UIViewController {
 
     }
 }
+
+extension DetailContactViewController {
+    // MARK: - Button Actions
+        @IBAction func messageButtonTapped(_ sender: UIButton) {
+            showAlert(title: "Message", message: "Messaging \(contact?.name ?? "this contact").")
+        }
+        
+        @IBAction func callButtonTapped(_ sender: UIButton) {
+            showAlert(title: "Call", message: "Calling \(contact?.name ?? "this contact").")
+        }
+        
+        @IBAction func videoButtonTapped(_ sender: UIButton) {
+            showAlert(title: "Video", message: "Starting video call with \(contact?.name ?? "this contact").")
+        }
+        
+        @IBAction func mailButtonTapped(_ sender: UIButton) {
+            showAlert(title: "Mail", message: "Sending mail to \(contact?.name ?? "this contact").")
+        }
+        
+        private func showAlert(title: String, message: String) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true)
+        }
+}
--- ContactApp/KeypadViewController.swift
+++ ContactApp/KeypadViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class KeypadViewController: UIViewController {
 
-    @IBOutlet weak var numberLabel: UILabel! // Label to display entered numbers
-
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var buttonMult: UIButton!
+    @IBOutlet weak var buttonHash: UIButton!
+    @IBOutlet weak var buttonBack: UIButton!
+    @IBOutlet weak var buttonCall: UIButton!
+    
     var enteredNumber: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        styleButtons()
+    }
+    private func styleButtons() {
+        let buttons: [UIButton] = [button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonCall, buttonMult, buttonHash, buttonBack]
+
+        for button in buttons {
+            button.layer.cornerRadius = button.frame.size.width / 2
+            button.clipsToBounds = true
+        }
     }
 
     @IBAction func numberButtonTapped(_ sender: UIButton) {
